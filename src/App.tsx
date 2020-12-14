f2b82847cc0c9a1e95db6c4923b8dc23114fe90d
turkaytunc <turkaytunc00@gmail.com>
2020-12-14 20:34:13 +0300

    refactor: css=>scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import "./App.css";
+import React, { ReactNode } from "react";
+import "./app.scss";
 import { Store } from "./state-store/Store";
 
 function App(): JSX.Element {
   const store = React.useContext(Store);
 
-  return <main className="App">{console.log(store) as any}</main>;
+  return <main className="App">{console.log(store) as ReactNode}</main>;
 }
 
 export default App;
