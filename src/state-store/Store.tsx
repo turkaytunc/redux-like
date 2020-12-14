a66ecbb86dda91a8d0da3e8504723413a5415094
turkaytunc <turkaytunc00@gmail.com>
2020-12-14 20:18:27 +0300

    add: post type for context provider

--- src/state-store/Store.tsx
+++ src/state-store/Store.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 
-const initialState = {};
+interface IPost {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+interface IPosts {
+  posts: Array<IPost>;
+}
+
+const initialState: IPosts = {
+  posts: [{ userId: 0, id: 0, title: "", body: "" }],
+};
 
 const reducer = () => {};
 
-export const Store = React.createContext(initialState);
+export const Store = React.createContext<IPosts>(initialState);
 
 export const StoreProvider = (props: any): JSX.Element => {
-  return (
-    <Store.Provider value={"initialvalue"}>{props.children}</Store.Provider>
-  );
+  return <Store.Provider value={initialState}>{props.children}</Store.Provider>;
 };
