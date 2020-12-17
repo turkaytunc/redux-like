3c992e7db5f7fca292c2cab287345e87599c75cb
turkaytunc <turkaytunc00@gmail.com>
2020-12-17 15:46:56 +0300

    refactor: out post list code as a component

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,15 @@
 import React from 'react';
 import './app.scss';
+import PostList from './components/post-list/PostList';
 import UserList from './components/user-list/UserList';
-import { Store } from './state-store/Store';
 
 function App(): JSX.Element {
-  const { state, dispatch } = React.useContext(Store);
-  const [input, setInput] = React.useState('');
-
-  const addPost = () => {
-    const parsed = JSON.parse(input);
-    if (parsed.hasOwnProperty('id')) {
-      dispatch({
-        type: 'ADD_POST',
-        payload: parsed,
-      });
-    }
-  };
-
-  const deletePost = (id: number) => {
-    dispatch({
-      type: 'REMOVE_POST',
-      payload: id,
-    });
-  };
-
   return (
     <main className="App">
       <UserList />
       <hr />
       <br />
-      <div className="post-container">
-        <input
-          type="text"
-          value={input}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setInput(e.target.value)}
-        />
-        <button onClick={() => addPost()}>Add Post</button>
-        {state.posts.map((e) => (
-          <div key={e.id}>
-            <div>title: {e.title}</div>
-            <hr />
-            <div>{e.body}</div>
-            <button onClick={() => deletePost(e.id)} style={{ fontSize: '2em' }}>
-              &times;
-            </button>
-          </div>
-        ))}
-      </div>
+      <PostList />
     </main>
   );
 }
--- src/components/post-list/PostList.tsx
+++ src/components/post-list/PostList.tsx
@@ -1,7 +1,47 @@
 import React, { FC } from 'react';
+import { Store } from '../../state-store/Store';
 
 const PostList: FC = () => {
-  return <div></div>;
+  const { state, dispatch } = React.useContext(Store);
+  const [input, setInput] = React.useState('');
+
+  const addPost = () => {
+    const parsed = JSON.parse(input);
+    if (parsed.hasOwnProperty('id')) {
+      dispatch({
+        type: 'ADD_POST',
+        payload: parsed,
+      });
+    }
+  };
+
+  const deletePost = (id: number) => {
+    dispatch({
+      type: 'REMOVE_POST',
+      payload: id,
+    });
+  };
+
+  return (
+    <div className="post-container">
+      <input
+        type="text"
+        value={input}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setInput(e.target.value)}
+      />
+      <button onClick={() => addPost()}>Add Post</button>
+      {state.posts.map((e) => (
+        <div key={e.id}>
+          <div>title: {e.title}</div>
+          <hr />
+          <div>{e.body}</div>
+          <button onClick={() => deletePost(e.id)} style={{ fontSize: '2em' }}>
+            &times;
+          </button>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default PostList;
